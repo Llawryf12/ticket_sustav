b97079b42673df59258171230c0215e11edeb0c3
Laura Celcner <lcelcner@veleri.hr>
2026-09-02 11:45:02 +0200

    provjera smtp konfiguracije

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,5 +1,12 @@
 import nodemailer from 'nodemailer';
 
+console.log('SMTP konfiguracija:', {
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  user: process.env.EMAIL_USER,
+  passExists: Boolean(process.env.EMAIL_PASS)
+});
+
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
   port: Number(process.env.EMAIL_PORT) || 587,
